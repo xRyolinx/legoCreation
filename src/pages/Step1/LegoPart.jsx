36f08296d84ff3dadb0de51a9ab6d378643083ad
Ryolin <manilrabia@gmail.com>
2024-10-17 08:58:51 +0100

    scale commented

--- src/pages/Step1/LegoPart.jsx
+++ src/pages/Step1/LegoPart.jsx
@@ -3,29 +3,29 @@ import { useRef, useEffect } from "react"
 
 const LegoPart = ({ large, src, alt, classNameImg }) => {
     // ref
-    const ref = useRef(null);
+    // const ref = useRef(null);
 
-    // on load svg¸
-    useEffect(() => {
-        if (! ref.current) {
-            return
-        }
+    // // on load svg¸
+    // useEffect(() => {
+    //     if (! ref.current) {
+    //         return
+    //     }
 
-        const img = ref.current
-        const scale = large ? 1 : 0.8
-        let height = img.naturalHeight * scale
-        let width = img.naturalWidth * scale
+    //     const img = ref.current
+    //     const scale = large ? 1 : 0.8
+    //     let height = img.naturalHeight * scale
+    //     let width = img.naturalWidth * scale
 
-        const oldMarginTop = parseFloat(window.getComputedStyle(img).marginTop);
-        const marginTop = oldMarginTop * scale
+    //     const oldMarginTop = parseFloat(window.getComputedStyle(img).marginTop);
+    //     const marginTop = oldMarginTop * scale
 
-        img.style.height = `${height}px`
-        img.style.width = `${width}px`
-        img.style.marginTop = `${marginTop}px`
-    }, [ref, large])
+    //     img.style.height = `${height}px`
+    //     img.style.width = `${width}px`
+    //     img.style.marginTop = `${marginTop}px`
+    // }, [ref, large])
 
     return (
-        <img ref={ref} src={src} alt={alt} className={`${classNameImg} h-full`}
+        <img src={src} alt={alt} className={`${classNameImg} h-full`}
             style={{}} />
     )
 }
